ec26f85a86aa5a9b68307187338b7541df944a61
vishukha <vishukha@github.com>
2015-09-20 22:43:57 +0530

    Integration #6

--- src/server/update_result.h
+++ src/server/update_result.h
@@ -2,10 +2,12 @@
 
 class Result
 {
-   double mean_final;
+   int mean_final;
    int mean_elements;
+   int min_final;
+   int max_final; 
 
-   Result() { mean_final = 0; mean_elements = 0;}
+   Result() { mean_final = 0; mean_elements = 0; min_final = 0; max_final = 0;}
    static Result *result;
    Result ( Result const& ) {};
    Result& operator = ( Result const& ) {};
@@ -17,19 +19,25 @@ class Result
        return result;
    }
 
-   void update_result (int task, int num, double answer)
+   void update_result (int task_id, int num1, int num2)
    {
+       task = db->get_opcode(task_id);
        switch (task)
        {
           case MEAN :
-             update_mean (num, answer);
+             update_mean (num1, num2);
+             break;
+
+          case RANGE :
+             update_range (num1, num2);
              break;
 
           default : ; // some error handling
        }
    }
 
-   void update_mean (int num, double answer);
-   double get_mean () {return mean_final;}
+   void update_mean (int num1, int num2);
+   void update_range (int num1, int num2);
+   int get_mean () {return mean_final;}
    int get_mean_elements () {return mean_elements;}
 };
