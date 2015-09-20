a9386d719a3c767a783d8ec672a84b91a86a13d0
Lakshmi <laksd@cisco.com>
2015-09-20 03:16:51 -0700

    server_changes

---
+++ server/multicast_server.h
@@ -0,0 +1,36 @@
+typedef enum multicast_group_ {
+    MEAN,
+    MEDIAN,
+    MODE
+} multicast_group_t;
+
+struct g_server_sock_t 
+{
+    uint32_t server_sock;
+    struct sockaddr_in server_addr;
+};
+    
+struct client_info_t 
+{
+    char client_ip[INET6_ADDRSTRLEN];
+    uint32_t client_id;
+    uint32_t multicast_group_id;
+};
+
+struct client_sock_fd_t
+{
+    uint32_t client_sock;
+    char     client_ip[INET6_ADDRSTRLEN];
+    uint32_t client_id;
+};
+
+struct client_sock_fd_t g_client_sock_info[1000];
+
+
+struct g_server_sock_t server_sock_hndl;
+void *register_client(struct client_sock_fd_t *);
+
+
+
+
+
