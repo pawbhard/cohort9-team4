43aaf18d741d7be5257deada85eb61f62c29f97f
VinodKumarLogan <vinodkumarlogan@gmail.com>
2015-09-20 17:18:56 +0530

    Updated client to receive and process int data

--- src/client/client_task_execute.cpp
+++ src/client/client_task_execute.cpp
@@ -1,22 +1,23 @@
 #include "../../include/client.h"
 
-static void calculate_mean(double data[], uint size, double result[]) {
+static void calculate_mean(int data[], int size, int result[]) {
 	double mean = data[0], diff;
-	uint i;
+	int i;
 	for (i=1;i<size;i++) {
 		diff = data[i] - mean;
 		mean += diff/(i+1);
 	}
-	result[1] = mean;
+	result[1] = (int)mean;
+	result[2] = size;
 }
 
-static void calculate_standard_deviation(double data[], uint size, double result[]) {
+static void calculate_standard_deviation(int data[], int size, int result[]) {
 
 }
 
-static void calculate_range(double data[], uint size, double result[]) {
-	double min = 0.0f, max = 0.0f;
-	uint i;
+static void calculate_range(int data[], int size, int result[]) {
+	int min = 0, max = 0;
+	int i;
 	for (i=0;i<size;++i) {
 		if (data[i]<min) {
 			min = data[i];
@@ -32,18 +33,18 @@ static void calculate_range(double data[], uint size, double result[]) {
 void *client_receive_and_execute_task (void *v_connection) {
 	bool flag = TRUE;
 	connection_handler* connection = (connection_handler*) v_connection; 
-	double data[BUFFER_SIZE];
-	double result[RESULT_SIZE];
+	int data[BUFFER_SIZE];
+	int result[RESULT_SIZE];
 	task_type task;
-	uint input_size = 0;
-	double *input;
+	int input_size = 0;
+	int *input;
 	
 	do {
 		std::cout << get_date_time() << " Waiting for data from server\n";
 		read (connection->local_fd, data, BUFFER_SIZE);
-		result[0] = data[0];
-		task = (task_type)data[1];
-		input_size = (uint)data[2];
+		result[0] = data[1];
+		task = (task_type)data[2];
+		input_size = (int)data[0];
 		input = data+3;
     	switch (task) {
     		case mean: 		std::cout << get_date_time() << " Calculating mean of %d elements\n";
